4d2d339642fb433be1c74bdab7aa6209a11922d0
Akshay Venkatesan <venkatesan.akshay@gmail.com>
2020-10-26 16:31:33 -0700

    48 cases / 52

--- maps/src/maps/ArrayMap.java
+++ maps/src/maps/ArrayMap.java
@@ -12,8 +12,6 @@ import java.util.NoSuchElementException;
 public class ArrayMap<K, V> extends AbstractIterableMap<K, V> {
     private static final int DEFAULT_INITIAL_CAPACITY = 10; // Thinking 10 because eventually implement mod operator
     private int size;
-    // private Object key
-    // private Object value
     /*
     Warning:
     You may not rename this field or change its type.
@@ -30,12 +28,6 @@ public class ArrayMap<K, V> extends AbstractIterableMap<K, V> {
     public ArrayMap(int initialCapacity) {
         this.entries = this.createArrayOfEntries(initialCapacity);
         size = 0;
-        // Is it a map yet??? When I call the methods am I directly changing the map?
-        // Or do I somehow need to link said array to the map.
-        // Do i implement a resize method?
-        // Hmm.. Iterator? Iterator<Entry<K, V>> iter = entries.iterator()
-        // key = null;
-        // value = null;
     }
 
     /**
@@ -64,7 +56,7 @@ public class ArrayMap<K, V> extends AbstractIterableMap<K, V> {
     @Override
     public V get(Object key) {
         for (int i = 0; i < size; i++) {
-            if (entries[i].getKey() == key) {
+            if (java.util.Objects.equals(entries[i].getKey(), key)) {
                 return entries[i].getValue();
             }
         }
@@ -76,18 +68,14 @@ public class ArrayMap<K, V> extends AbstractIterableMap<K, V> {
         if (size == entries.length) {
             entries = resize();
         }
-        // if (size == 0) { // inserting (key, value) at the first index since map is size of 0
-        //     entries[0] = new SimpleEntry<>(key, value);
-        // } else {
-            for (int i = 0; i < size; i++) { // if Key exists
-                if (key != null && entries[i].getKey() == key) {
-                    V oldVal = entries[i].getValue();
-                    entries[i].setValue(value);
-                    return oldVal;
-                }
+        for (int i = 0; i < size; i++) { // if Key exists
+            if (key != null && java.util.Objects.equals(entries[i].getKey(), key)) {
+                V oldVal = entries[i].getValue();
+                entries[i].setValue(value);
+                return oldVal;
             }
-            entries[size] = new SimpleEntry<>(key, value);
-        //}
+        }
+        entries[size] = new SimpleEntry<>(key, value);
         size++;
         return null;
     }
@@ -95,7 +83,7 @@ public class ArrayMap<K, V> extends AbstractIterableMap<K, V> {
     @Override
     public V remove(Object key) {
         for (int i = 0; i < size; i++) {
-            if (key != null && entries[i].getKey() == key) {
+            if (key != null && java.util.Objects.equals(entries[i].getKey(), key)) {
                 V removedV = entries[i].getValue();
                 entries[i] = entries[size - 1];
                 entries[size - 1] = null;
@@ -116,7 +104,7 @@ public class ArrayMap<K, V> extends AbstractIterableMap<K, V> {
     @Override
     public boolean containsKey(Object key) {
         for (int i = 0; i < size; i++) {
-            if (entries[i].getKey() == key) {
+            if (java.util.Objects.equals(entries[i].getKey(), key)) {
                 return true;
             }
         }
@@ -145,12 +133,6 @@ public class ArrayMap<K, V> extends AbstractIterableMap<K, V> {
         return new ArrayMapIterator<>(this.entries);
     }
 
-    // // Doing so will give you a better string representation for assertion errors the debugger.
-    // @Override
-    // public String toString() {
-    //     return super.toString();
-    // }
-
     private static class ArrayMapIterator<K, V> implements Iterator<Map.Entry<K, V>> {
         private final SimpleEntry<K, V>[] entries;
         // You may add more fields and constructor parameters
@@ -163,7 +145,6 @@ public class ArrayMap<K, V> extends AbstractIterableMap<K, V> {
 
         @Override
         public boolean hasNext() {
-            //return (entries[currIndex + 1] != null);
             return ((entries.length - 1 > currIndex) && (entries[currIndex] != null));
         }
 
@@ -176,7 +157,6 @@ public class ArrayMap<K, V> extends AbstractIterableMap<K, V> {
             } else {
                 throw new NoSuchElementException();
             }
-
         }
     }
 }
