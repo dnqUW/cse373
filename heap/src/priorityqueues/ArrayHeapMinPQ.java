b232e383d206978b557e0dd9fcc9a5a7bb6bb442
Derek Q <dnq@uw.edu>
2020-11-16 17:27:41 -0800

    49/49

--- heap/src/priorityqueues/ArrayHeapMinPQ.java
+++ heap/src/priorityqueues/ArrayHeapMinPQ.java
@@ -1,13 +1,10 @@
 package priorityqueues;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.stream.IntStream;
 
 /**
  * @see ExtrinsicMinPQ
