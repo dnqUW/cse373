e28245e00e78ca19ac309ec49b3fd854146582c9
Derek <dnq@uw.edu>
2020-12-02 13:57:12 -0800

    14/31

--- mazes/src/graphs/shortestpaths/DijkstraShortestPathFinder.java
+++ mazes/src/graphs/shortestpaths/DijkstraShortestPathFinder.java
@@ -5,7 +5,13 @@ import graphs.Graph;
 import priorityqueues.DoubleMapMinPQ;
 import priorityqueues.ExtrinsicMinPQ;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * Computes shortest paths using Dijkstra's algorithm.
@@ -31,14 +37,47 @@ public class DijkstraShortestPathFinder<G extends Graph<V, E>, V, E extends Base
 
     @Override
     protected Map<V, E> constructShortestPathsTree(G graph, V start, V end) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        Map<V, E> spt =  new HashMap<>();
+        Map<V, Double> distTo = new HashMap<>();
+        distTo.put(start, 0.0);
+        ExtrinsicMinPQ<V> pq = new DoubleMapMinPQ<>();
+        pq.add(start, 0);
+        while (!pq.isEmpty()) {
+            V u = pq.removeMin();
+            for (E edge : graph.outgoingEdgesFrom(u)) {
+                double oldDist;
+                oldDist = distTo.getOrDefault(edge.to(), Double.POSITIVE_INFINITY);
+                double newDist = distTo.get(u) + edge.weight();
+                if (newDist < oldDist) {
+                    distTo.put(edge.to(), newDist);
+                    spt.put(edge.to(), edge);
+                    if (pq.contains(edge.to())) {
+                        pq.changePriority(edge.to(), newDist);
+                    } else {
+                        pq.add(edge.to(), newDist);
+                    }
+                }
+            }
+        }
+        return spt;
     }
 
     @Override
     protected ShortestPath<V, E> extractShortestPath(Map<V, E> spt, V start, V end) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        E edge = spt.get(end);
+        if (spt.get(edge) == null) {
+            return new ShortestPath.Failure<>();
+        }
+        if (start.equals(end)) {
+            return new ShortestPath.SingleVertex<>(end);
+        }
+
+        List<E> list = new ArrayList<>(spt.size());
+        while (edge.from() != null) {
+            list.add(edge);
+            edge = spt.get(edge.from());
+        }
+        return new ShortestPath.Success<>(list);
     }
 
 }
