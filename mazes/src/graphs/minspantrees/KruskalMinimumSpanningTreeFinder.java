e08a2c128070ae475ed94b43e7c7a2e8c6cecad1
Derek <dnq@uw.edu>
2020-12-04 10:57:14 -0800

    ON Kruskal's

--- mazes/src/graphs/minspantrees/KruskalMinimumSpanningTreeFinder.java
+++ mazes/src/graphs/minspantrees/KruskalMinimumSpanningTreeFinder.java
@@ -4,10 +4,15 @@ import disjointsets.DisjointSets;
 import disjointsets.QuickFindDisjointSets;
 import graphs.BaseEdge;
 import graphs.KruskalGraph;
+import graphs.shortestpaths.ShortestPath;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * Computes minimum spanning trees using Kruskal's algorithm.
@@ -38,10 +43,38 @@ public class KruskalMinimumSpanningTreeFinder<G extends KruskalGraph<V, E>, V, E
         // sort edges in the graph in ascending weight order
         List<E> edges = new ArrayList<>(graph.allEdges());
         edges.sort(Comparator.comparingDouble(E::weight));
-
+        List<V> vertices = new ArrayList<>(graph.allVertices());
         DisjointSets<V> disjointSets = createDisjointSets();
+        // Set<E> finalMST = new HashSet<>();
+        // So if we get two islands, disconnected, or if one vertex is not conncetd to the MST
+        // If check for failure if (any vertices are disconnected, fail)
+        // As long as all vertices are connected, you can find an MST Success
+        // for (E edge : edges) {
+        //     V u = edge.from();
+        //     V v = edge.to();
+        //     if (u == null && v == null) {
+        //         return new MinimumSpanningTree.Failure<>();
+        //     }
+        //     int uMST = disjointSets.findSet(u);
+        //     int vMST = disjointSets.findSet(v);
+        //     if (uMST != vMST) {
+        //         finalMST.add(edge);
+        //         disjointSets.union(u, v);
+        //     }
+        // }
 
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        for (E edge : edges) {
+            V u = edge.from();
+            V v = edge.to();
+            // finalMST.add(edge);
+            if (disjointSets.findSet(u) != disjointSets.findSet(v)) {
+
+            }
+            // edges.remove(edge);
+        }
+        if (edges.size() == vertices.size() - 1) { // reduce the size of the list edges
+            return new MinimumSpanningTree.Success<>();
+        }
+        return new MinimumSpanningTree.Failure<>();
     }
 }
