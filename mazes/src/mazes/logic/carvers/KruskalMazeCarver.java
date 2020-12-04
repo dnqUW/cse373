992a41d20579ba3e0b318b81bd4273689c8ff032
Derek <dnq@uw.edu>
2020-12-04 15:20:30 -0800

    on chooseWallsToRemove

--- mazes/src/mazes/logic/carvers/KruskalMazeCarver.java
+++ mazes/src/mazes/logic/carvers/KruskalMazeCarver.java
@@ -1,13 +1,20 @@
 package mazes.logic.carvers;
 
+import graphs.BaseEdge;
 import graphs.EdgeWithData;
+import graphs.Graph;
+import graphs.minspantrees.MinimumSpanningTree;
 import graphs.minspantrees.MinimumSpanningTreeFinder;
 import mazes.entities.Room;
 import mazes.entities.Wall;
 import mazes.logic.MazeGraph;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Carves out a maze based on Kruskal's algorithm.
@@ -31,9 +38,19 @@ public class KruskalMazeCarver extends MazeCarver {
 
     @Override
     protected Set<Wall> chooseWallsToRemove(Set<Wall> walls) {
-        // TODO: replace this with your code
         // Hint: you'll probably need to include something like the following:
+        Set<Wall> removed = new HashSet<>();
+        List<EdgeWithData<Room, Wall>> edges = walls.stream()
+            .map(wall -> new EdgeWithData<>(wall.getRoom1(), wall.getRoom2(), wall.getDistance(), wall))
+            .collect(Collectors.toList());
+        // for (Wall wall : walls) {
+        //     wall = new EdgeWithData<>(wall.getRoom1(), wall.getRoom2(), wall.getDistance(), wall);
+        // for (Wall wall : walls) {
+        //     wall = new EdgeWithData<>(wall.getRoom1(), wall.getRoom2(), wall.getDistance(), wall);
+        //
+        // }
+
         // this.minimumSpanningTreeFinder.findMinimumSpanningTree(new MazeGraph(edges));
-        throw new UnsupportedOperationException("Not implemented yet.");
+        return removed;
     }
 }
