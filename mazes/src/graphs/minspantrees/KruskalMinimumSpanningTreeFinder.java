16448242c1e59827c3399c0b7b9472fa3b551ece
Derek <dnq@uw.edu>
2020-12-04 14:14:06 -0800

    Finished KMST findMin

--- mazes/src/graphs/minspantrees/KruskalMinimumSpanningTreeFinder.java
+++ mazes/src/graphs/minspantrees/KruskalMinimumSpanningTreeFinder.java
@@ -38,59 +38,27 @@ public class KruskalMinimumSpanningTreeFinder<G extends KruskalGraph<V, E>, V, E
 
     @Override
     public MinimumSpanningTree<V, E> findMinimumSpanningTree(G graph) {
-        // Here's some code to get you started; feel free to change or rearrange it if you'd like.
-
-        // sort edges in the graph in ascending weight order
         List<E> edges = new ArrayList<>(graph.allEdges());
-        edges.sort(Comparator.comparingDouble(E::weight));
         List<V> vertices = new ArrayList<>(graph.allVertices());
         DisjointSets<V> disjointSets = createDisjointSets();
         Set<E> finalMST = new HashSet<>();
-
-        // So if we get two islands, disconnected, or if one vertex is not conncetd to the MST
-        // If check for failure if (any vertices are disconnected, fail)
-        // As long as all vertices are connected, you can find an MST Success
-
+        if (vertices.size() <= 1) {
+            return new MinimumSpanningTree.Success<>();
+        }
         for (V vertex : vertices) {
             disjointSets.makeSet(vertex); // processed vertex
         }
+        edges.sort(Comparator.comparingDouble(E::weight));
         for (E edge : edges) {
-            if (disjointSets.findSet(edge.from()) != disjointSets.findSet(edge.to())) {
+            V u = edge.from();
+            V v = edge.to();
+            if (disjointSets.union(u, v)) {
                 finalMST.add(edge);
-                disjointSets.union(edge.from(), edge.to());
-            }
-            if (edges.size() == vertices.size() - 1) { // reduce the size of the list edges
-                return new MinimumSpanningTree.Success<>(finalMST);
+                if (finalMST.size() == vertices.size() - 1) {
+                    return new MinimumSpanningTree.Success<>(finalMST);
+                }
             }
         }
-
-
-        // for (E edge : edges) {
-        //     V u = edge.from();
-        //     V v = edge.to();
-        //     if (u == null && v == null) {
-        //         return new MinimumSpanningTree.Failure<>();
-        //     }
-        //     int uMST = disjointSets.findSet(u);
-        //     int vMST = disjointSets.findSet(v);
-        //     if (uMST != vMST) {
-        //         finalMST.add(edge);
-        //         disjointSets.union(u, v);
-        //     }
-        // }
-
-        // for (E edge : edges) {
-        //     V u = edge.from();
-        //     V v = edge.to();
-        //     // finalMST.add(edge);
-        //     if (disjointSets.findSet(u) != disjointSets.findSet(v)) {
-        //
-        //     }
-        //     // edges.remove(edge);
-        // }
-        // if (edges.size() == vertices.size() - 1) { // reduce the size of the list edges
-        //     return new MinimumSpanningTree.Success<>();
-        // }
         return new MinimumSpanningTree.Failure<>();
     }
 }
