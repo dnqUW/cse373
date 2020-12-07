a7c2b87eeae263a20bb244d8e0969b747eeaec22
Derek <dnq@uw.edu>
2020-12-07 13:41:19 -0800

    10/12 UnionBySizeCompressingDisjointSets.java

--- mazes/src/disjointsets/UnionBySizeCompressingDisjointSets.java
+++ mazes/src/disjointsets/UnionBySizeCompressingDisjointSets.java
@@ -1,6 +1,11 @@
 package disjointsets;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * A quick-union-by-size data structure with path compression.
@@ -8,33 +13,54 @@ import java.util.List;
  */
 public class UnionBySizeCompressingDisjointSets<T> implements DisjointSets<T> {
     // Do NOT rename or delete this field. We will be inspecting it directly in our private tests.
-    List<Integer> pointers;
-
+    List<Integer> pointers; // related, still have pointers
+    private Map<T, Integer> ids;
+    private int size;
     /*
     However, feel free to add more fields and private helper methods. You will probably need to
     add one or two more fields in order to successfully implement this class.
     */
 
     public UnionBySizeCompressingDisjointSets() {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        pointers = new ArrayList<>();
+        ids = new HashMap<>();
+        size = 0;
     }
 
     @Override
     public void makeSet(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        pointers.add(-1);
+        ids.put(item, this.size);
+        size++;
     }
 
     @Override
     public int findSet(T item) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        Integer index = this.ids.get(item);
+        if (index == null) {
+            throw new IllegalArgumentException(item + " is not in any set.");
+        }
+        // Returns the index of the overall parent
+        // record where we have visited
+        Set<Integer> set = new HashSet<>();
+        while (pointers.get(index) > 0) {
+            set.add(index);
+            index = pointers.get(index);
+        }
+        for (Integer i : set) {
+            pointers.set(i, index);
+        }
+        return index;
     }
 
     @Override
     public boolean union(T item1, T item2) {
-        // TODO: replace this with your code
-        throw new UnsupportedOperationException("Not implemented yet.");
+        int id1 = findSet(item1);
+        int id2 = findSet(item2);
+        if (id1 == id2) {
+            return false;
+        }
+        this.ids.replaceAll((item, rep) -> rep == id1 ? id2 : rep);
+        return true;
     }
 }
