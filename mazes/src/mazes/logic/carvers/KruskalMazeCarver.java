cbe6f3bc5378f32e61df0a3fd4281ef8e99e4371
Derek <dnq@uw.edu>
2020-12-07 12:07:55 -0800

    finished chooseWallsToRemove, and abided to checkstyle

--- mazes/src/mazes/logic/carvers/KruskalMazeCarver.java
+++ mazes/src/mazes/logic/carvers/KruskalMazeCarver.java
@@ -1,15 +1,15 @@
 package mazes.logic.carvers;
 
-import graphs.BaseEdge;
+// import graphs.BaseEdge;
 import graphs.EdgeWithData;
-import graphs.Graph;
+// import graphs.Graph;
 import graphs.minspantrees.MinimumSpanningTree;
 import graphs.minspantrees.MinimumSpanningTreeFinder;
 import mazes.entities.Room;
 import mazes.entities.Wall;
 import mazes.logic.MazeGraph;
 
-import java.util.Collection;
+// import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
@@ -39,18 +39,16 @@ public class KruskalMazeCarver extends MazeCarver {
     @Override
     protected Set<Wall> chooseWallsToRemove(Set<Wall> walls) {
         // Hint: you'll probably need to include something like the following:
-        Set<Wall> removed = new HashSet<>();
         List<EdgeWithData<Room, Wall>> edges = walls.stream()
-            .map(wall -> new EdgeWithData<>(wall.getRoom1(), wall.getRoom2(), wall.getDistance(), wall))
+            .map(wall -> new EdgeWithData<>(wall.getRoom1(), wall.getRoom2(), rand.nextDouble(), wall))
             .collect(Collectors.toList());
-        // for (Wall wall : walls) {
-        //     wall = new EdgeWithData<>(wall.getRoom1(), wall.getRoom2(), wall.getDistance(), wall);
-        // for (Wall wall : walls) {
-        //     wall = new EdgeWithData<>(wall.getRoom1(), wall.getRoom2(), wall.getDistance(), wall);
-        //
-        // }
-
-        // this.minimumSpanningTreeFinder.findMinimumSpanningTree(new MazeGraph(edges));
+        Set<Wall> removed = new HashSet<>();
+        MinimumSpanningTree<Room, EdgeWithData<Room, Wall>> mst =
+            this.minimumSpanningTreeFinder.findMinimumSpanningTree(new MazeGraph(edges));
+        for (EdgeWithData<Room, Wall> wall : mst.edges()) {
+            removed.add(wall.data());
+        }
         return removed;
     }
+
 }
